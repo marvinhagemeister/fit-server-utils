9b690462c0ccf2522f4467ca031f8746dde240e6
Marvin Hagemeister <marvin@marvinhagemeister.de>
2016-09-22 19:50:22 +0200

    Increase timeout for travis

--- test/typescript.spec.ts
+++ test/typescript.spec.ts
@@ -1,7 +1,9 @@
 import * as tt from "typescript-definition-tester";
 
 describe("TypeScript definitions", () => {
-  it("should compile against index.d.ts", done => {
+  // tslint:disable-next-line:non-arrow
+  it("should compile against index.d.ts", function(done) {
+    this.timeout(5000);
     tt.compileDirectory(
       __dirname + "/../src",
       (fileName: string) => fileName.match(/\.ts$/),
